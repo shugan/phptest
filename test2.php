4b2415e486d8838424e99114f526341caee7f5c6
shugan <shugan_80@yahoo.co.in>
2013-04-14 01:41:56 +0530

    Update test2.php
    JavaScript validation implemented for integer validation.

--- test2.php
+++ test2.php
@@ -59,7 +59,41 @@ if(isset($_POST['submit'])) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script type="text/javascript" src="js/test.js"></script>
+<script language="javascript">
+function Validate(){
+     var start_range = document.range.start_range;
+     var end_range = document.range.end_range;	 
+	if ( start_range.value == ""){
+	  alert("Kindly enter the start range");
+	  start_range.focus();
+	  return false;
+	}
+	if ( start_range.value!= ""){
+	    if ( !isInteger(start_range.value) ){
+			alert("Kindly enter the valid start range");
+			start_range.focus();
+			return false;
+		}
+	}
+	if ( end_range.value == ""){
+	  alert("Kindly enter the end range");
+	  end_range.focus();
+	  return false;
+	}
+	if ( end_range.value!= ""){
+	    if ( !isInteger(end_range.value) ){
+			alert("Kindly enter the valid end range");
+			end_range.focus();
+			return false;
+		}
+	}
+	return true;
+}
+
+function isInteger(val) {
+   return /^\d+$/.test(val);
+}
+</script>
 <link rel="stylesheet" href="css/test.css">
 <title>Test2</title>
 </head>
